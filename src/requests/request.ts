e8978d8980f5e6911983922c10e56eb237cf17cf
Andrei Garbo <andrei.garbo@gmail.com>
2026-07-05 20:17:45 +0300

    checkpoint

--- src/requests/request.ts
+++ src/requests/request.ts
@@ -5,13 +5,29 @@ enum HTTP_METHOD {
 
 class Request {
   method: HTTP_METHOD;
-  target: string;
+  location: string;
+  module: string;
+  endpoint: string;
   params?: string[];
 
-  constructor(method: HTTP_METHOD, target: string, params?: string[]) {
+  constructor(method: HTTP_METHOD, module: string, endpoint: string, params?: string[]) {
     this.method = method;
-    this.target = target;
+    this.module = module;
+    this.endpoint = endpoint;
     this.params = params;
+    this.location = import.meta.env.SERVER_URL;
+  }
+
+  public dispatch() {}
+
+  private buildURL(module: string, endpoint: string): string {
+    return this.location + '/' + module + '/' + endpoint;
+  }
+
+  private async get(module: string, endpoint: string, params?: string): Promise<Response> {
+    const requestUrl = this.buildURL(module, endpoint);
+
+    let response = null;
   }
 }
 
